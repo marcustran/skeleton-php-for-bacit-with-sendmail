c4686012c3afee3db2b14c8e52521522391ae919
Janis Gailis <janis.gailis@uia.no>
2020-09-10 09:08:47 +0200

    fjerner sens data

--- public/mail.php
+++ public/mail.php
@@ -2,12 +2,12 @@
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
     $from = "test@gmail.com";
-    $to = "uia.community@gmail.com";
+    $to = "engyldigkonto@gmail.com";
     $subject = "PHP Mail Test script";
     $message = "This is a test to check the PHP Mail functionality";
     //$headers = "From:" . $from;
     $headers = 'From:' . $from . "\r\n" .
-    'Reply-To: uia.community@gmail.com' . "\r\n" .
+    'Reply-To: engyldigkonto@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
     mail($to,$subject,$message, $headers);
     echo "Test email sent with exp headers";
